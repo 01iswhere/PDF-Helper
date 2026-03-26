a3b3a3bff9b36f455651d4d2fe9f9a7be3150c47
Zero1isWhere <chenkkkkk0324@163.com>
2026-03-26 16:14:35 +0800

    初始化项目

---
+++ src/main/java/com/zero1iswhere/pdfhelper/PdfHelperApplication.java
@@ -0,0 +1,13 @@
+package com.zero1iswhere.pdfhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero1iswhere/pdfhelper/PdfHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero1iswhere.pdfhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
